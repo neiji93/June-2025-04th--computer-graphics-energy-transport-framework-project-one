2ac12ae5ead1e5c5c62dcc0d1d9e5f2e0f3b75b2
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-06-04 16:23:37 +0200

    Update Cputdbrdf.h

--- Cputdbrdf.h
+++ Cputdbrdf.h
@@ -1,1 +1,8 @@
 //only visible lights.
+
+//yhe duhamel principle seems not intuitive but this is not the case for strong knowledge support processes 
+//then we can approximate many things (light behaviour) by 
+//using nowsdays techniques. lets use a common shadow mao technoc
+//without ai. then, we can ise computer graphics technisues for cutting edge light transmort
+//for tdbrdf 
+
